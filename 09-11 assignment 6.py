70c41d49963468d264c7dae47f6b71cb036b66e7
ANORA\vinoth <vinoth@anorasolutions.com>
2023-03-21 16:09:48 +0530

    remove psuedocode

--- 09-11 assignment 6.py
+++ 09-11 assignment 6.py
@@ -1,8 +1,3 @@
 c=[100,50,25,10]
 l1=[1.8*x+32 for x in c]
 print(l1)
-""" 
-pseudocode:
-1.take a list in celsius
-2.celcius is converted into fahreheit by formula f=(c*1.8)+32 in list comphrehension
-"""
